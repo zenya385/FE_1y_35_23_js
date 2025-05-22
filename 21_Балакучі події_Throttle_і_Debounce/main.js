c2081aba30ef92a21cd72919ce3e9bb984470969
Roman Zeno <zeno385@gmail.com>
2025-05-22 20:53:34 +0300

    throttle debounce

--- 21_Балакучі події_Throttle_і_Debounce/main.js
+++ 21_Балакучі події_Throttle_і_Debounce/main.js
@@ -1,6 +1,12 @@
+// ==========================mousemove===================================
+
+// document.addEventListener("mousemove",  _.throttle(() => {}, 300));
+
 const mouseMove = document.querySelector(".js-event");
 
-document.addEventListener("mousemove", onMouseMove);
+// document.addEventListener("mousemove", onMouseMove, 300);
+document.addEventListener("mousemove", _.throttle(onMouseMove, 300));
+// document.addEventListener("mousemove", _.debounce(onMouseMove, 300));
 
 let mouseCount = 0;
 
@@ -11,3 +17,29 @@ function onMouseMove(event) {
   //   const { clientX, clientY } = event;
   //   console.log(`Mouse moved to: (${clientX}, ${clientY})`);
 }
+
+// ===========================input==================================
+
+const input = document.querySelector("input[type='text']");
+const output = document.querySelector(".js-output");
+
+let inputCount = 0;
+
+// input.addEventListener("input", onInputChange);
+// input.addEventListener("input", _.debounce(onInputChange, 500));
+input.addEventListener("input", _.throttle(onInputChange, 300));
+
+function onInputChange(e) {
+  inputCount += 1;
+  output.textContent = `Кількість викликів onInputChange: ${inputCount}, значення input: ${e.target.value}`;
+}
+
+// ===========================scroll==================================
+
+const outputScroll = document.querySelector(".output");
+let scrollEventCounter = 0;
+
+document.addEventListener("scroll", () => {
+  scrollEventCounter += 1;
+  outputScroll.textContent = scrollEventCounter;
+});
