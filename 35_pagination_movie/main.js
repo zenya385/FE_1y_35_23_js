4fd5afa21e3ec1603c0067dc8740a7766a3fbcaf
Roman Zeno <zeno385@gmail.com>
2025-09-11 21:02:29 +0300

    fix

--- 35_pagination_movie/main.js
+++ 35_pagination_movie/main.js
@@ -3,6 +3,9 @@ const loadMore = document.querySelector(".js-load");
 
 const BASE_URL = "https://api.themoviedb.org/3/";
 const ENDPOINT = "trending/movie/day";
+const API_KEY =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWIwY2JiODdhYmNiY2U5MjY1MTJhYWNmMmNmODVjMiIsIm5iZiI6MTY0MjA3NTcyMC4wMzIsInN1YiI6IjYxZTAxNjQ4YzE3NWIyMDA0MWNjODZlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-K3dXXWqGpHSXtjGmQtaJYQ3Gm1p86tWI22t5nlgEq8";
+
 let currentPage = 1;
 
 loadMore.addEventListener("click", onLoad);
@@ -26,15 +29,14 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWIwY2JiODdhYmNiY2U5MjY1MTJhYWNmMmNmODVjMiIsIm5iZiI6MTY0MjA3NTcyMC4wMzIsInN1YiI6IjYxZTAxNjQ4YzE3NWIyMDA0MWNjODZlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-K3dXXWqGpHSXtjGmQtaJYQ3Gm1p86tWI22t5nlgEq8",
+    Authorization: `Bearer ${API_KEY}`,
   },
 };
 
 function getTrending(page = 1) {
   return fetch(
     //   "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
-    `${BASE_URL}${ENDPOINT}?page=${page}$language=uk-UA`,
+    `${BASE_URL}${ENDPOINT}?page=${page}&language=uk-UA`,
     options
   ).then((res) => {
     if (!res.ok) {
@@ -64,7 +66,7 @@ function createMarkup(arr) {
         original_title,
         release_date,
       }) =>
-        `<li>
+        `<li class="list-item">
     <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}">
        <h2>${title} / ${original_title}</h2>
       <h2>Рейтинг: ${vote_average}</h2>
